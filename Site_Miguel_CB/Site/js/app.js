1f6360b818e1021b1e175f209bb4b977a294c748
ZeluisFS <zeluisfs@hotmail.com>
2015-12-20 23:22:04 +0000

    Html Js para a web ja feitos. Só falta acertar CSS
    Para a web já so falta acertar medidas. Tudo operacional

--- Site_Miguel_CB/Site/js/app.js
+++ Site_Miguel_CB/Site/js/app.js
@@ -5,32 +5,134 @@ routerApp.config(function($stateProvider, $urlRouterProvider) {
   $urlRouterProvider.otherwise('/homeopatia');
 
   $urlRouterProvider.when('', '/homeopatia'); //Quando vai parar ao index html, automaticamente vai para o estado homeopatia
+  $urlRouterProvider.when('/questoesFrequentes', '/questoesFrequentes/questao1');
 
   $stateProvider
 
     .state('homeopatia', {
       url: '/homeopatia',
-      templateUrl: 'homeopatia.html'
+      templateUrl: 'homeopatia.html',
+      onEnter: function() {
+         var logo = document.getElementById("nav1");
+        logo.src = "img/header/botaoHomeopatiaOn.png";
+      },
+      onExit: function() {
+         var logo = document.getElementById("nav1");
+        logo.src = "img/header/botaoHomeopatiaOff.png";
+      }
     })
 
     .state('quemSou', {
       url: '/quemSou', 
-      templateUrl: 'quemSou.html'
+      templateUrl: 'quemSou.html',
+      onEnter: function() {
+         var logo = document.getElementById("nav2");
+        logo.src = "img/header/botaoQuemSouOn.png";
+      },
+      onExit: function() {
+         var logo = document.getElementById("nav2");
+        logo.src = "img/header/botaoQuemSouOff.png";
+      }
     })
+
+    .state('questoesFrequentes', {
+      url: '/questoesFrequentes',
+      templateUrl: 'questoesMain.html',
+      onEnter: function() {
+         var logo = document.getElementById("nav3");
+        logo.src = "img/header/botaoQuestoesOn.png";
+      },
+      onExit: function() {
+         var logo = document.getElementById("nav3");
+        logo.src = "img/header/botaoQuestoesOff.png";
+      }
+    })
+
+      //Nested Views from questoesFrequentes
+        .state('questoesFrequentes.questao1', {
+          url: '/questao1',
+          templateUrl: 'questoesFrequentes/questao1.html'
+        })
+
+        .state('questoesFrequentes.questao2', {
+          url: '/questao2',
+          templateUrl: 'questoesFrequentes/questao2.html'
+        })
+
+        .state('questoesFrequentes.questao3', {
+          url: '/questao3',
+          templateUrl: 'questoesFrequentes/questao3.html'
+        })
+
+        .state('questoesFrequentes.questao4', {
+          url: '/questao4',
+          templateUrl: 'questoesFrequentes/questao4.html'
+        })
+
+        .state('questoesFrequentes.questao5', {
+          url: '/questao5',
+          templateUrl: 'questoesFrequentes/questao5.html'
+        })
+
+        .state('questoesFrequentes.questao6', {
+          url: '/questao6',
+          templateUrl: 'questoesFrequentes/questao6.html'
+        })
+
+        .state('questoesFrequentes.questao7', {
+          url: '/questao7',
+          templateUrl: 'questoesFrequentes/questao7.html'
+        })
+
+        .state('questoesFrequentes.questao8', {
+          url: '/questao8',
+          templateUrl: 'questoesFrequentes/questao8.html'
+        })
+      //End of nested views
 
     .state('contacto', {
       url: '/contacto', 
-      templateUrl: 'contacto.html'
+      templateUrl: 'contacto.html',
+      onEnter: function() {
+         var logo = document.getElementById("nav4");
+        logo.src = "img/header/botaoContactosOn.png";
+      },
+      onExit: function() {
+         var logo = document.getElementById("nav4");
+        logo.src = "img/header/botaoContactosOff.png";
+      }
     })
 
     .state('ondeEstou', {
       url: '/ondeEstou', 
-      templateUrl: 'ondeEstou.html'
+      templateUrl: 'ondeEstou.html',
+      onEnter: function() {
+         var logo = document.getElementById("nav5");
+        logo.src = "img/header/botaoOndeEstouOn.png";
+      },
+      onExit: function() {
+         var logo = document.getElementById("nav5");
+        logo.src = "img/header/botaoOndeEstouOff.png";
+      }
     })
 
     .state('blog', {
       url: '/blog', 
-      templateUrl: 'blog.html'
+      templateUrl: 'blog.html',
+      onEnter: function() {
+         document.getElementById("nav6").src = 
+         "img/header/botaoBlogOn.png";
+
+        //Vamos fazer aparecer o footer do Blog
+        document.getElementById("footerBlog").style.display = "block";
+      },
+      onExit: function() {
+         document.getElementById("nav6").src =
+         "img/header/botaoBlogOff.png";
+
+        //Vamos fazer desaparecer o footer do Blog
+        document.getElementById("footerBlog").style.display = "none";
+      }
     });
 
 } );
